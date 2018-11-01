ad53161c3794f5640a3c20bcefc57b25ad3b1b80
professionalrose20022002 <professionalrose20022002@gmail.com>
2018-11-01 14:05:23 -0400

    Update Magic-Chicken-Dare-You GAME-Project.py

---
+++ Magic-Chicken-Dare-You GAME-Project.py
@@ -0,0 +1,46 @@
+#import the modules
+import sys
+import random
+
+ans = True
+
+while ans:
+    question = raw_input("Dare-you tell the Truth and ask the magic 10 ball:(press enter to quit)")
+    words = ["Chicken", "Dare You"]
+    answers = random.randint(1,10)
+    randword = random.randint(0,1)
+    word = words[randword]
+    print(word)
+
+    if question =="":
+        sys.exit()
+
+    elif answers == 1:
+        print"If Chicken,then eat ketchup"
+
+    elif answers == 2:
+        print"If Chicken,DareYou to hop on one foot"
+
+    elif answers == 3:
+        print"DareYou,Smell you feet"
+
+    elif answers == 4:
+        print"Laugh-out Loud"
+
+    elif answers == 5:
+        print"DareYou,Balance book on head,standing on one foot"
+
+    elif answers == 6:
+        print"Wiggle your Hips"
+
+    elif answers == 7:
+        print"Tap Your Toes"
+
+    elif answers == 8:
+        print"DareYou,Pick Your Nose"
+
+    elif answers == 9:
+        print"If Chicken,Eat a donut"
+
+    elif answers == 10:
+        print"Have fun"
